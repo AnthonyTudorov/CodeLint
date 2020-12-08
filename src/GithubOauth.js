0e0190ebf7c51ef49fbc146993e41a58552bef67
Rudra Desai <rudradesai09@gmail.com>
2020-12-08 12:55:30 -0500

    Changed redirect uri, changed http to https

--- src/GithubOauth.js
+++ src/GithubOauth.js
@@ -10,5 +10,5 @@ export default function GithubOauth() {
       state,
     });
     window.location = `${'https://github.com/login/oauth/authorize?client_id=862222f107a099fa6750&redirect_uri='
-            + 'http://codelint.herokuapp.com/&state='}${state}&scope=repo`;
+            + 'https://codelint.herokuapp.com/&state='}${state}&scope=repo`;
 }
